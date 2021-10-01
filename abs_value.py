5174d2753377743190902d1a4fbf1f847419e565
calebbeswick <beswica@ssfs.org>
2021-10-01 12:34:38 -0400

    Worked on If statements in unit 4

---
+++ abs_value.py
@@ -0,0 +1,10 @@
+
+def absolute_value(x):
+    answer = abs(x)
+    print("The absolute value of", x, "is", answer)
+
+def main():
+    x = int(input("Please enter the number "))
+    absolute_value(x)
+
+main()
