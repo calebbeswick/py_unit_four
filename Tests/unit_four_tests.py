c74b1392b04f9b76759d2c5336a8e1789513c8a1
calebbeswick <beswica@ssfs.org>
2021-10-12 11:36:24 -0400

    Assignment 4

--- Tests/unit_four_tests.py
+++ Tests/unit_four_tests.py
@@ -5,36 +5,37 @@ import triangles
 import grades
 import rock_paper_scissors
 import assignment_four
-from abs_value import absolute_value
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_is_divisible(self):
-        self.assertTrue(divisible.is_divisible(30, 15))
-        self.assertFalse(divisible.is_divisible(4, 9))
+    # def test_is_divisible(self):
+    #     self.assertTrue(divisible.is_divisible(30, 15))
+    #     self.assertFalse(divisible.is_divisible(4, 9))
+    #
+    # def test_is_triangle(self):
+    #     self.assertTrue(triangles.is_triangle(7, 11, 16))
+    #     self.assertFalse(triangles.is_triangle(20, 7, 11))
+    #     self.assertFalse(triangles.is_triangle(4, 9, 3))
+    #     self.assertFalse(triangles.is_triangle(9, 13, 22))
+    #
+    # def test_points_1(self):
+    #     self.assertEqual(2, grades.points(80, False))
+    #
+    # def test_points_2(self):
+    #     self.assertEqual(0.2, grades.points(64, True))
+    #
+    # def test_points_3(self):
+    #     self.assertEqual(3, grades.points(88, False))
 
-    def test_is_triangle(self):
-        self.assertTrue(triangles.is_triangle(7, 11, 16))
-        self.assertFalse(triangles.is_triangle(20, 7, 11))
-        self.assertFalse(triangles.is_triangle(4, 9, 3))
-        self.assertFalse(triangles.is_triangle(9, 13, 22))
+    # def test_rps(self):
+    #     self.assertEqual("You win!", rock_paper_scissors.who_wins(3, 2))
+    #     self.assertEqual("It is a tie", rock_paper_scissors.who_wins(1, 1))
 
-    def test_points_1(self):
-        self.assertEqual(2, grades.points(80, False))
+    def test_get_type(self):
+        test = assignment_four.get_sign()
+        self.assertTrue(test == "*" or test == "-" or test == "*")
 
-    def test_points_2(self):
-        self.assertEqual(0.2, grades.points(64, True))
-
-    def test_points_3(self):
-        self.assertEqual(3, grades.points(88, False))
-
-
-    def test_rps(self):
-        self.assertEqual("You win!", rock_paper_scissors.who_wins(3, 2))
-        self.assertEqual("It is a tie", rock_paper_scissors.who_wins(1, 1))
-
-    
 
 if __name__ == '__main__':
     unittest.main()
--- assignment_four.py
+++ assignment_four.py
@@ -5,16 +5,26 @@ import random
 
 
 
-def generating_numbers():
-    maximum = int(input("Please enter the maximum number of the equation "))
-    minimum = int(input("Please enter the minimum number of the equation "))
-    sign = input("Please enter the sign of the question. (+, *, or -) ")
+def generating_numbers(maximum, minimum):
     if maximum >= 20:
         maximum = 20
     elif minimum <= 1:
         minimum = 1
     num1 = random.randint(minimum, maximum)
-    num2 = random.randint(minimum, maximum)
+    return num1
+
+
+def get_sign():
+    x = input("Please enter the sign ")
+
+    if x == "*":
+        return "*"
+    elif x == "-":
+        return "-"
+    else:
+        return "+"
+
+def getting_sign(sign, num1, num2):
     if sign == "*":
         question = num1 * num2
         print(num1, "*", num2)
@@ -32,11 +42,16 @@ def generating_numbers():
         print("You got it right!")
     else:
         print("You are incorrect")
+    return sign
 
 
 
 
-generating_numbers()
-
-
+def main():
+    maximum = int(input("Please enter the maximum number of the equation "))
+    minimum = int(input("Please enter the minimum number of the equation "))
+    sign = get_sign()
+    num1 = generating_numbers(minimum, maximum)
+    num2 = generating_numbers(minimum, maximum)
+    getting_sign(sign, num1, num2)
 
